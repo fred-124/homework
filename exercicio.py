bfdfd3b93844dc9a733f6b4b3189e60c175049c2
fred-124 <fredericocunha131314@gmail.com>
2025-09-19 19:53:07 -0300

    exercicio.py

---
+++ exercicio.py
@@ -0,0 +1,8 @@
+def cidadeEPais(cidade,pais='Brasil'):
+    print(cidade+' está localizada no '+pais)
+
+cidadeEPais('São Paulo')
+
+cidadeEPais('Rio de Janeiro')
+
+cidadeEPais('Nova York','Estados Unidos')
