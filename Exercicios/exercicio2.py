aa1197e676014fc1df9c6406f1de5eb917a24491
fred-124 <fredericocunha131314@gmail.com>
2025-09-19 20:02:22 -0300

    Update exercicio2.py

--- Exercicios/exercicio2.py
+++ Exercicios/exercicio2.py
@@ -1,1 +1,12 @@
+def corrida(corredor1distancia,corredor1tempo,corredor2distancia,corredor2tempo):
+    corredor1velocidade = corredor1distancia/corredor1tempo
+    corredor2velocidade = corredor2distancia/corredor2tempo
+    if corredor1velocidade > corredor2velocidade:
+     print("o corredor 1 é o mais rapido")
+    else:
+       print("o corredor 2 é o mais rapido")
 
+
+
+corrida(2,3,5,7)
+corrida(3,1,5,7)
